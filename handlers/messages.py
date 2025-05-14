132f97c13d9c77ad024831def68ec476ffed06e5
Rey-Hash <rey142911@gmail.com>
2025-05-14 07:48:35 +0630

    Update messages.py

--- handlers/messages.py
+++ handlers/messages.py
@@ -1,6 +1,9 @@
 from telegram import Update
 from telegram.ext import ContextTypes, MessageHandler, filters
 
+if update.message:
+    await update.message.reply_text(update.message.text)
+
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(update.message.text)
 
